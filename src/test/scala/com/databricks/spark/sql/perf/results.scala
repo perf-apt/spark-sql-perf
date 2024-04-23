8d297b262b5ad1c6a7798d3171140e992b5584eb
ashahid <ashahid@cloudera.com>
2024-04-23 12:06:34 -0700

    local changhe

--- src/test/scala/com/databricks/spark/sql/perf/results.scala
+++ src/test/scala/com/databricks/spark/sql/perf/results.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.functions._
 val resultLocation = "/tmp/tpcds_results"
-val result = spark.read.json(resultLocation).filter("timestamp = 1699071333431").select(explode($"results").as("r"))
+val result = spark.read.json(resultLocation).filter("timestamp = 1708201741783").select(explode($"results").as("r"))
 result.createOrReplaceTempView("result")
-spark.sql("select  r.name, bround((r.parsingTime+r.analysisTime+r.optimizationTime+r.planningTime+r.executionTime)/1000.0,1) as Runtime_sec  from result").show(100)
+spark.sql("select   bround((r.parsingTime+r.analysisTime+r.optimizationTime+r.planningTime+r.executionTime)/1000.0,1) as Runtime_sec  from result").show(100)
 
