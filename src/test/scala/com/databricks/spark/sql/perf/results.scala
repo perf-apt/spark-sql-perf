825de222c5c6f1cf2743cffa30bb1a626d39e03a
ashahid <ashahid@cloudera.com>
2025-03-23 21:09:49 -0700

    temp

--- src/test/scala/com/databricks/spark/sql/perf/results.scala
+++ src/test/scala/com/databricks/spark/sql/perf/results.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.functions._
-val resultLocation = "/tmp/tpcds_results"
-val result = spark.read.json(resultLocation).filter("timestamp = 1708201741783").select(explode($"results").as("r"))
+val resultLocation = "/data/tpcds_benchmark/tpcds_results"
+val result = spark.read.json(resultLocation).filter("timestamp=1742708607240").select(explode($"results").as("r"))
 result.createOrReplaceTempView("result")
-spark.sql("select   bround((r.parsingTime+r.analysisTime+r.optimizationTime+r.planningTime+r.executionTime)/1000.0,1) as Runtime_sec  from result").show(100)
+spark.sql("select   bround((r.parsingTime+r.analysisTime+r.optimizationTime+r.planningTime+r.executionTime)/1000.0,1) as Runtime_sec  from result").show(50)
+
 
